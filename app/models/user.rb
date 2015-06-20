f1d0c18046a6a9bccfd6eda0b38ce4645423fb96
Alex Knoll <alexandergknoll@gmail.com>
2015-06-20 17:32:51 -0500

    add uniqueness validation for phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :phone, uniqueness: true
 
   phony_normalize :phone, :default_country_code => 'US'
 end
