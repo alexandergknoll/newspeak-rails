ce907e151d16a8398c48d54228d5dec12ba11b5b
Alex Knoll <alexandergknoll@gmail.com>
2015-06-20 23:16:25 -0500

    add user json api

---
+++ app/controllers/api_users_controller.rb
@@ -0,0 +1,26 @@
+class ApiUsersController < ApplicationController
+  # before_filter :parse_request, :authenticate_client_from_token!
+  before_filter :find_user, only: [:show]
+
+  def show
+    render json: @user
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by_phone(PhonyRails.normalize_number(params[:phone], :country_code => 'US'))
+    render nothing: true, status: :not_found unless @user.present?
+  end
+
+  def authenticate_client_from_token!
+    unless @json['api_token'] == ENV["API_KEY"]
+      render nothing: true, status: :unauthorized
+    end
+  end
+
+  def parse_request
+    json = request.body.read
+    @json = JSON.parse(json)
+  end
+end
