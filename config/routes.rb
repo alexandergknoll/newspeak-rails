5e6cbe3cd82dd27111b4fbb605290c7563a4391c
Morgan McCrory <morganmccrory@gmail.com>
2015-06-21 00:53:53 -0500

    Fix broken user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   devise_scope :user do
     get "sign_in", to: "devise/sessions#new"
     get "sign_out", to: "devise/sessions#destroy"
-    get "registration", to: "devise/registrations#new"
   end
   resources :users
   resources :headlines
