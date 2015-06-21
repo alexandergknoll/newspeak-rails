3e38d2733fd2dffeea361b769dad64272024e522
Morgan McCrory <morganmccrory@gmail.com>
2015-06-20 21:37:55 -0500

    Add category params

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -3,7 +3,11 @@ require 'net/http'
 class HeadlinesController < ApplicationController
 
   def index
-    url = URI.parse('http://api.nytimes.com/svc/news/v3/content/all/u.s./.json?api-key=sample-key')
+    if params[:categories]
+      url = URI.parse("http://api.nytimes.com/svc/news/v3/content/all/#{params[:categories]}/.json?api-key=sample-key")
+    else
+      url = URI.parse("http://api.nytimes.com/svc/news/v3/content/all/u.s./.json?api-key=sample-key")
+    end
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
